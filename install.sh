a78aeca750d62dc49bba280661382b68affbd39c
Casey Witt <casey.witt@gmail.com>
2021-05-03 12:32:25 +0000

    working nextcloud - next to do auto configuration (insttead of first login), and letsencrypt certs

--- install.sh
+++ install.sh
@@ -70,6 +70,8 @@ setup_host() {
 		IFACE='${IFACE:="eth0"}'
 		# network interface prefix in the container
 		IFACE_PREFIX='${IFACE_PREFIX:="mv-"}'
+		# nextcloud tarball
+		NEXTCLOUD='${NEXTCLOUD:="https://download.nextcloud.com/server/releases/nextcloud-21.0.1.zip"}'
 	EOF
 
 	log "installing alpine linux ..."
@@ -157,6 +159,9 @@ setup_host() {
 		--settings=false \
 		--console=pipe \
 		--setenv="SCRIPT_ENV=CONTAINER" \
+		--setenv="HOSTNAME=$HOSTNAME" \
+		--setenv="DOMAIN=$DOMAIN" \
+		--setenv="NEXTCLOUD=$NEXTCLOUD" \
 		sh -s < "$0"
 
 	log "finished"
@@ -165,6 +170,7 @@ setup_host() {
 setup_container() {
 	# this function is meant to be run INSIDE the container (not the host)
 	# it is run automatically at the end of the setup_host function
+	# this is adapted from https://wiki.alpinelinux.org/wiki/Nextcloud
 	echo 'in the container'
 
 	rc-update add networking boot
@@ -174,9 +180,11 @@ setup_container() {
 	rc-update add killprocs shutdown
 	rc-update add savecache shutdown
 
+	log 'installing postgresql ...'
 	apk add postgresql postgresql-contrib postgresql-openrc
-	rm "/etc/conf.d/postgresql"
 	export PGDATA='/var/lib/postgresql/data'
+	echo "data_dir=\"$PGDATA\"" >> "/etc/conf.d/postgresql"
+	sed -i '/^conf_dir/ s/^/#/' "/etc/conf.d/postgresql"
 	su postgres -c initdb
 	adduser -HDS nextcloud
 	su postgres -c "pg_ctl -o '-k /tmp' start"
@@ -188,6 +196,108 @@ setup_container() {
 	EOF
 	su postgres -c 'pg_ctl stop'
 	rc-update add postgresql
+
+	log 'installing nextcloud tarball ...'
+	apk add curl # use curl since wget wants to use IPV6
+	mkdir -p "/usr/share/webapps"
+	curl -L "$NEXTCLOUD" | busybox unzip -d "/usr/share/webapps" -
+
+
+	log 'installing nginx and php7 ...'
+	apk add nginx php7 php7-fpm \
+		ffmpeg \
+		php7-bz2 \
+		php7-common \
+		php7-ctype \
+		php7-curl \
+		php7-dom \
+		php7-exif \
+		php7-fileinfo \
+		php7-gd \
+		php7-gmp \
+		php7-iconv \
+		php7-intl \
+		php7-json \
+		php7-mbstring \
+		php7-opcache \
+		php7-openssl \
+		php7-pdo \
+		php7-pdo_pgsql \
+		php7-pecl-imagick \
+		php7-pgsql \
+		php7-posix \
+		php7-session \
+		php7-simplexml \
+		php7-xml \
+		php7-xmlreader \
+		php7-xmlwriter \
+		php7-zip
+	# DO THE FOLLOWING TO /etc/nginx.conf
+	# REMOVE TLSv1.1 from /etc/nginx/nginx.conf (probably with sed)
+	# ENABLE gzipping of responses (I think)
+
+	mv "/etc/nginx/http.d/default.conf" "/etc/nginx/http.d/default.conf.orig"
+	cat > "/etc/nginx/http.d/$HOSTNAME.$DOMAIN.conf" <<-EOF
+		server {
+		  #listen       [::]:80; #uncomment for IPv6 support
+		  listen       80;
+		  return 301 https://\$host\$request_uri;
+		  server_name $HOSTNAME.$DOMAIN;
+		}
+
+		server {
+		  #listen       [::]:443 ssl; #uncomment for IPv6 support
+		  listen       443 ssl;
+		  server_name  $HOSTNAME.$DOMAIN;
+
+		  root /usr/share/webapps/nextcloud;
+		  index  index.php index.html index.htm;
+		  disable_symlinks off;
+
+		  ssl_certificate      /etc/ssl/nginx.crt;
+		  ssl_certificate_key  /etc/ssl/nginx.key;
+		  ssl_session_timeout  5m;
+
+		  #Enable Perfect Forward Secrecy and ciphers without known vulnerabilities
+		  #Beware! It breaks compatibility with older OS and browsers (e.g. Windows XP, Android 2.x, etc.)
+		  #ssl_ciphers ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA;
+		  #ssl_prefer_server_ciphers  on;
+
+
+		  location / {
+		    try_files \$uri \$uri/ /index.html;
+		  }
+
+		  # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+		  location ~ [^/]\.php(/|$) {
+		    fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+		    if (!-f \$document_root\$fastcgi_script_name) {
+		      return 404;
+		    }
+		    fastcgi_pass 127.0.0.1:9000;
+		    #fastcgi_pass unix:/run/php-fpm/socket;
+		    fastcgi_index index.php;
+		    include fastcgi.conf;
+		  }
+		}
+	EOF
+
+	sed -i '/^user =/ s/.*/user = nginx/' "/etc/php7/php-fpm.d/www.conf"
+	sed -i '/^group =/ s/.*/group = www-data/' "/etc/php7/php-fpm.d/www.conf"
+	sed -i 's/^;env/env/' "/etc/php7/php-fpm.d/www.conf"
+
+	rc-update add nginx
+	rc-update add php-fpm7
+
+	log 'creating self signed certificate ...'
+	apk add openssl
+	openssl req -x509 \
+		-nodes \
+		-days 365 \
+		-newkey rsa:4096 \
+		-subj "/CN=$HOSTNAME.$DOMAIN" \
+		-keyout /etc/ssl/nginx.key \
+		-out /etc/ssl/nginx.crt
 
 	log "finished installing nextcloud"
 }
