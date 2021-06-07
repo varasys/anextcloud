33be23bc56244fe23dc0e4a3e12c649419f7487a
Casey Witt <casey.witt@gmail.com>
2021-06-07 11:43:20 -0500

    updated self_signed_cert.sh script

--- self_signed_cert.sh
+++ self_signed_cert.sh
@@ -6,9 +6,10 @@ FQDN="${1:-"${FQDN:="${HOSTNAME:="$(hostname -s)"}.${DOMAIN:="$(hostname -d)"}"}
 printf 'creating self signed certificate for: "%s" ...\n\n' "$FQDN"
 openssl req -x509 \
 	-nodes \
-	-days 365 \
+	-days "${DAYS:="365"}" \
 	-newkey ec \
 	-pkeyopt ec_paramgen_curve:prime256v1 \
 	-out "$FQDN.crt" \
-	-keyout "$FQDN.key"
+	-keyout "$FQDN.key" \
+	-subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=${CN:=$FQDN}/emailAddress=$emailAddress"
 chmod 600 "$FQDN.key"
