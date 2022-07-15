4fc9f159cf3f61bf9d6ace9a24ac7b53eadedeca
Casey Witt <kcwitt@gmail.com>
2022-07-15 14:20:18 -0500

    added zpool.sh script

---
+++ zpool.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -eu
+
+POOL="${1:?missing pool name argument}"
+shift
+if [ $# -lt 1 ]; then
+	printf "missing device argument(s)\n" >&2
+	exit 1
+fi
+exit 3
+
+sudo zpool create \
+	-o ashift=12 \
+	-o autotrim=on \
+	-O acltype=posixacl \
+	-O mountpoint=none \
+	-O compression=on \
+	-O dnodesize=auto \
+	-O normalization=formD \
+	-O relatime=on \
+	-O xattr=sa \
+	-O encryption=on \
+	-O keylocation=prompt \
+	-O keyformat=passphrase \
+	"$POOL" \
+	"$@"
